d414edd851d87bb57576ef50622855d12d83e9d0
EPro30 <82483823+EPro30@users.noreply.github.com>
2021-09-13 10:01:37 -0400

    Update exArea.java

--- src/main/java/exArea.java
+++ src/main/java/exArea.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 public class exArea {
     public static void main(String[] args)
     {
+        final double constRate = 0.09290304;
         Scanner sc = new Scanner(System.in);
         System.out.println("What is the length of the room in feet?");
         int len = sc.nextInt();
         System.out.println("What is the length of the room in feet?");
         int width = sc.nextInt();
         int ftArea = len*width;
-        double mArea = ftArea* 0.09290304;
+        double mArea = ftArea*constRate;
         System.out.println("You entered dimension of " + len + " feet by " + width + " feet.");
         System.out.println("The area is");
         System.out.println(ftArea + " square feet");
